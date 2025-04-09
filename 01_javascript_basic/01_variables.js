5a95cee771cb31ea142d1f8d852c8cea5ccc1811
Muhammad Huzaifa <151584255+H2wenty@users.noreply.github.com>
2025-04-09 23:51:29 +0000

    new javascript variable file

---
+++ 01_javascript_basic/01_variables.js
@@ -0,0 +1,11 @@
+const id = 122;
+let name = 'Huzaifa';
+var password = '1jdd';
+city = 'karachi'; 
+
+// id = 334;
+// console.log(id);
+
+name = 'muhammad';
+let state;
+console.table([id,name,password,city,state])
