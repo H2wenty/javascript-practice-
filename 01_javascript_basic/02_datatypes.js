1a5d545c8ddf6cf8207bf97c2307e9c70707c73d
Muhammad Huzaifa <151584255+H2wenty@users.noreply.github.com>
2025-04-10 22:59:10 +0000

    new  javascript file datatypes

---
+++ 01_javascript_basic/02_datatypes.js
@@ -0,0 +1,14 @@
+let name = "huzaifa";
+console.log(typeof name +" "+ name);
+
+let age = 22;
+console.log(typeof age +" "+ age);
+
+let isMale = true;
+console.log(typeof isMale +" "+ isMale);
+
+let state;
+console.log(typeof state +" "+ state);
+
+let city = null;
+console.log(typeof city+" "+city);
