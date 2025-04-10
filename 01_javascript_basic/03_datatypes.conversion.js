58dbf4a388f52eae8433d3b522b9052608519f7f
Muhammad Huzaifa <151584255+H2wenty@users.noreply.github.com>
2025-04-10 23:53:03 +0000

    new js dts conversions

---
+++ 01_javascript_basic/03_datatypes.conversion.js
@@ -0,0 +1,66 @@
+//Conveting every datatype into number. 
+// let age ="22";
+// console.log(age +" "+ typeof age);
+
+// let convertString = Number(age);
+// console.log(convertString +" "+ typeof convertString);
+
+// let a ="22vh";
+// console.log(a +" "+ typeof a);
+
+// let convertaInNumber = Number(a);
+// console.log(convertaInNumber +" "+ typeof convertaInNumber);
+
+// let b =null;
+// console.log(b +" "+ typeof b);
+
+// let convertbInNumber = Number(b);
+// console.log(convertbInNumber +" "+ typeof convertbInNumber);
+
+// let c =undefined;
+// console.log(c +" "+ typeof c);
+
+// let convertcInNumber = Number(c);
+// console.log(convertcInNumber +" "+ typeof convertcInNumber);
+
+// let d =true;
+// console.log(d +" "+ typeof d);
+
+// let convertdInNumber = Number(d);
+// console.log(convertdInNumber +" "+ typeof convertdInNumber);
+
+//Conveting every datatype into string.
+
+// let age =22;
+// console.log(age +" "+ typeof age);
+
+// let convertageString = String(age);
+// console.log(convertageString +" "+ typeof convertageString);
+
+
+
+// let b =null;
+// console.log(b +" "+ typeof b);
+
+// let convertbInString = String(b);
+// console.log(convertbInString +" "+ typeof convertbInString);
+
+// let c =undefined;
+// console.log(c +" "+ typeof c);
+
+// let convertcInString = String(c);
+// console.log(convertcInString +" "+ typeof convertcInString);
+
+// let d =true;
+// console.log(d +" "+ typeof d);
+
+// let convertdInString = String(d);
+// console.log(convertdInString +" "+ typeof convertdInString);
+
+//Conveting every datatype into boolean.
+
+let number  = 1;
+console.log(number +" "+ typeof number);
+
+let convertNumbertoBoolean = Boolean(number);
+console.log(convertNumbertoBoolean +" "+ typeof convertNumbertoBoolean);
