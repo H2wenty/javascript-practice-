ca9dc63bfdf84c52fb27dd453a61625b3ac7851d
Muhammad Huzaifa <151584255+H2wenty@users.noreply.github.com>
2025-04-17 05:17:59 +0000

    methods of string

---
+++ 01_javascript_basic/07_strings.js
@@ -0,0 +1,23 @@
+// const game = new String('Huzaifa');
+// console.log(game);
+// console.log(game[0]);
+// console.log(game.__proto__);
+
+// console.log(game.length)
+
+// let sentence ="hello my name is Huzaifa"
+// console.log(sentence.at(5));
+
+// console.log(sentence.charAt(6));
+
+// let password ="hu55it8#" 
+// console.log(password.charCodeAt(0)); 'Returns the unicode of any letter, number or symbol'
+// console.log(password.charCodeAt(1));
+// console.log(password.charCodeAt(2));
+// console.log(password.charCodeAt(3));
+// console.log(password.charCodeAt(4));
+// console.log(password.charCodeAt(5));
+// console.log(password.charCodeAt(6));
+// console.log(password.charCodeAt(7));
+// console.log(password.charCodeAt(8));
+
