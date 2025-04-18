fae30a23a113e8769fb6b378189601274dbe5dea
Muhammad Huzaifa <151584255+H2wenty@users.noreply.github.com>
2025-04-18 06:29:20 +0000

    Numbers and Math

---
+++ 01_javascript_basic/08_Numbers&Maths.js
@@ -0,0 +1,36 @@
+//******************************** NUMBERS ********************************** */
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const num =123.8977 
+// console.log(num.toPrecision(3))
+
+// const numbers=1000000;
+// console.log(numbers.toLocaleString())
+// console.log(numbers.toLocaleString("en-PK"))
+// console.log(numbers.toLocaleString("en-IN"))
+
+//******************************** MATHS ********************************** */
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.2));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(3,7,4,1,0,55)); 
+// console.log(Math.Max(3,7,4,1,0,55)); 
+
+// console.log(Math.random())
+// console.log(Math.random()*10)
+// console.log((Math.random()*10)+1)
+// console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min)
