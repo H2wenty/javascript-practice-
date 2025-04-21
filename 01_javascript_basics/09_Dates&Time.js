416cd59e3f93a686b63e8c914ff77dfa570c517d
Muhammad Huzaifa <151584255+H2wenty@users.noreply.github.com>
2025-04-21 22:57:37 +0000

    new changes

---
+++ 01_javascript_basics/09_Dates&Time.js
@@ -0,0 +1,37 @@
+// Dates
+const mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate)
+
+// const myCreatedDate = new Date(2025,2,20)
+// console.log(myCreatedDate.toDateString());
+
+// const myCreatedDate = new Date(2025,2,20,3,6)
+// console.log(myCreatedDate.toLocaleString())
+
+// const myCreatedDate = new Date("2025-03-20")
+// console.log(myCreatedDate.toLocaleString());
+
+// const myCreatedDate = new Date("03-20-2025")
+// console.log(myCreatedDate.toLocaleString());
+
+// let mytime = Date.now()
+
+// console.log(mytime);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getDate());
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
+// console.log(newDate.getMilliseconds());
+
+// let date = newDate.toLocaleString('default',{
+//     timeZone: 'Asia/Karachi'
+// })
+
+// console.log(date)
