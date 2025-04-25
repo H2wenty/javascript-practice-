3b3a345c24fa9e2ed5843d711383a6bc2c97a903
Muhammad Huzaifa <151584255+H2wenty@users.noreply.github.com>
2025-04-25 22:43:06 +0000

    javascript functions

---
+++ 03_javascript_basics/01_functions.js
@@ -0,0 +1,13 @@
+//Functions
+
+function name() {
+    console.log('h');
+    console.log('u')
+    console.log('z')
+    console.log('a')
+    console.log('i')
+    console.log('f')
+    console.log('a')
+}
+
+name()
