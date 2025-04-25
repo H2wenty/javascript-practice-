9e0005482b15a147af12a48cce48d1e888fb65d8
Muhammad Huzaifa <151584255+H2wenty@users.noreply.github.com>
2025-04-25 22:09:29 +0000

    objects in detailed

---
+++ 02_javascript_basics/04_Objects.js
@@ -0,0 +1,49 @@
+// const fb = new Object();
+// console.log(fb);
+
+const fbUser ={}
+
+fbUser.id = '001fbhuzaifa';
+fbUser.email='huzaifa@fb.com'
+// console.log(fbUser);
+
+const xUser = {
+    email:'moavia@xco',
+    userName:{
+        fullname:{
+            firstName:'Muhammad',
+            lastName:'Huzaifa'
+        }
+    }
+}
+
+// console.log(xUser.userName.fullname.firstName);
+
+const obj1 = {1:"a",2:"b"};
+const obj2 = {3:"c",4:"d"};
+const obj3 = {5:"e",6:"f"};
+
+// const combinedObj = {obj1,obj2,obj3};
+// const combinedObj = Object.assign({},obj1,obj2,obj3)
+const combinedObj = {...obj1,...obj2,...obj3}
+// console.log(combinedObj);
+
+const user=[
+    {
+        id:1,
+        email:"huzaifa@spaceX.com"
+    },
+    {
+        id:2,
+        email:"moavia@spaceX.com"
+    }
+]
+
+// console.log(user[1].email);
+// console.log(fbUser);
+// console.log(Object.keys(fbUser));
+// console.log(Object.values(fbUser));
+// console.log(Object.entries(fbUser));
+// console.log(fbUser.hasOwnProperty('email'));
+
+
