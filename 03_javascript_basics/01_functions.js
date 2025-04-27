c3edf40a8a6114fd2dbb2a0f245bbd62d1a003cc
Muhammad Huzaifa <151584255+H2wenty@users.noreply.github.com>
2025-04-27 23:41:52 +0000

    functiions features

--- 03_javascript_basics/01_functions.js
+++ 03_javascript_basics/01_functions.js
@@ -10,4 +10,37 @@ function name() {
     console.log('a')
 }
 
-name()
+// name()
+
+// function addNumbers(num1,num2){
+//     console.log(num1+num2);
+// }
+// addNumbers(3,7);
+
+function addNumbers(num1,num2){
+    // console.log(num1+num2); will return undefined
+    
+    // let result = num1+num2 
+    // return result;  will return the value
+    
+    return num1+num2; //short version
+}
+const result = addNumbers(3,7);
+// console.log(result);
+
+ function userLogin(name = 'moavia'){
+    if (!name) {
+        console.log('please enter correct name');
+        return
+    }
+    return `Welcome ${name}`
+ }
+// console.log(userLogin('Huzaifa'))
+// console.log(userLogin())
+
+function addTocartPrice(v1,v2,...num) {
+    return  num;
+}
+// console.log(addTocartPrice(100,3000,580));
+
+
