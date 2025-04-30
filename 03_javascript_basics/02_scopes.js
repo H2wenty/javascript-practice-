6160789ecc6774bdc176c7669e0bb7ada0116f25
Muhammad Huzaifa <151584255+H2wenty@users.noreply.github.com>
2025-04-30 20:13:16 +0000

    Scope level and Mini hoisting

--- 03_javascript_basics/02_scopes.js
+++ 03_javascript_basics/02_scopes.js
@@ -5,16 +5,50 @@ var c=300
 
 {
     let a=110
-    console.log('let inner ' +a);
+    // console.log('let inner ' +a);
     
     const b=210
-    console.log('const inner' +b);
+    // console.log('const inner' +b);
 
     var c=310
-    console.log('var inner ' +c);
+    // console.log('var inner ' +c);
 
 }
 
-console.log('let outer '+a);
-console.log('const outer '+b);
-console.log('var outer '+c);
+// console.log('let outer '+a);
+// console.log('const outer '+b);
+// console.log('var outer '+c);
+
+//Scope level
+function one(){
+    const username='Huzaifa'
+    
+    function two(){
+        const website='youtube'
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if(true){
+    const fName='Muhammad'
+    if(fName==='Muhammad'){
+        const lName='Huzaifa'
+        console.log(fName+' '+lName)
+    }
+    // console.log(lName);
+}
+// console.log(fName);
+
+//++++++++++++++++++++++++ Mini Hoisting +++++++++++++++++++++++++
+console.log(addOne(5));
+function addOne(num) {
+    return num + 1
+}
+
+console.log(addTwo())
+const addTwo = function addTwo(num) {
+    return num + 2
+} 
