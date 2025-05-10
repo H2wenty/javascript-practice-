b0ffb5c30780b9a2a3df3992bd5f43e35f3f4448
Muhammad Huzaifa <151584255+H2wenty@users.noreply.github.com>
2025-05-10 20:49:55 +0000

    forOf loops in javascript

---
+++ 05_iterations/03_forOfLoops.js
@@ -0,0 +1,24 @@
+// for of
+let number=[1,2,3,4,5];
+for (const num of number) {
+    // console.log(num);
+}
+
+const name='huzaifa';
+for (const char of name) {
+    // console.log(char);
+}
+
+//Maps
+
+const map = new Map()
+map.set('PK','Pakistan')
+map.set('TK','Turkey')
+map.set('PL','Palestine')
+map.set('PK','Pakistan')
+// console.log(map);
+
+// for (const [key , value] of map) {
+//     console.log(`${key} = ${value}`);
+// }
+
